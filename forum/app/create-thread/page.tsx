f57bcdffee25b6231c55e8f31fd8bd08f7b20261
cvhlpont <pontchvl@gmail.com>
2024-08-30 15:57:53 +0200

    update: error msg and navigation

--- forum/app/create-thread/page.tsx
+++ forum/app/create-thread/page.tsx
@@ -1,15 +1,22 @@
-'use client';
-import { useState } from 'react';
-import { getThreadsFromLocalStorage, saveThreadsToLocalStorage } from '@/utils/localStorage';
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import {
+  getThreadsFromLocalStorage,
+  saveThreadsToLocalStorage,
+} from "@/utils/localStorage";
 
 const CreateThread = () => {
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState<ThreadCategory>('THREAD');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState<ThreadCategory>("THREAD");
+  const [description, setDescription] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  const router = useRouter();
 
   const handleSubmit = () => {
     if (!title || !description) {
-      alert('Title and Description are required!');
+      setError("Fields cannot be empty");
       return;
     }
 
@@ -20,27 +27,33 @@ const CreateThread = () => {
       category,
       creationDate: new Date().toISOString(),
       description,
-      creator: { userName: 'guest', password: 'password' },
-      commentCount: 0
+      creator: { userName: "guest", password: "password" },
+      commentCount: 0,
     };
 
     threads.push(newThread);
     saveThreadsToLocalStorage(threads);
-    alert('Thread created successfully!');
 
-  
-    setTitle('');
-    setCategory('THREAD');
-    setDescription('');
+    setTitle("");
+    setCategory("THREAD");
+    setDescription("");
+    setError(null);
+
+    router.push(`/${newThread.id}`);
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-3xl font-extrabold text-center mb-8 text-gray-800">Create New Thread</h1>
+        <h1 className="text-3xl font-extrabold text-center mb-8 text-gray-800">
+          Create New Thread
+        </h1>
 
         <div className="mb-6">
-          <label htmlFor="title" className="block text-sm font-medium text-gray-700 mb-2">
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-700 mb-2"
+          >
             Title
           </label>
           <input
@@ -54,7 +67,10 @@ const CreateThread = () => {
         </div>
 
         <div className="mb-6">
-          <label htmlFor="category" className="block text-sm font-medium text-gray-700 mb-2">
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-700 mb-2"
+          >
             Category
           </label>
           <select
@@ -69,7 +85,10 @@ const CreateThread = () => {
         </div>
 
         <div className="mb-6">
-          <label htmlFor="description" className="block text-sm font-medium text-gray-700 mb-2">
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-700 mb-2"
+          >
             Description
           </label>
           <textarea
@@ -81,6 +100,9 @@ const CreateThread = () => {
             onChange={(e) => setDescription(e.target.value)}
           />
         </div>
+
+        {/* Display error message if exists */}
+        {error && <p className="text-red-500 mb-6 font-semibold">{error}</p>}
 
         <button
           className="w-full bg-blue-600 text-white py-3 rounded-md font-semibold hover:bg-blue-700 focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 transition-all"
